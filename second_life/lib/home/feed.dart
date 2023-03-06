656b03043bc5a8e09c19cb236209aeb4cd88f581
AzizBourmel1 <azizbourmel02@gmail.com>
2023-03-06 11:57:18 +0100

    some changes on the feed page

--- second_life/lib/home/feed.dart
+++ second_life/lib/home/feed.dart
@@ -29,7 +29,8 @@ class _feedState extends State<feed> {
                   prefixIcon: Icon(Icons.search),
                   label: Text("Search "),
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+                    borderSide: Colors.amber,
+                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
                 keyboardType: TextInputType.text,
@@ -40,126 +41,132 @@ class _feedState extends State<feed> {
               Container(
                 child: Column(
                   children: [
-                    Row(
-                      children: [
-                        Container(
-                          height: 280,
-                          width: 150,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.green),
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Color.fromARGB(255, 160, 232, 162)),
-                            ],
+                    Container(
+                      height: 350,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.green),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(color: Color.fromARGB(255, 160, 232, 162)),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Image(
+                            image: AssetImage("assets/login.png"),
                           ),
-                          child: Column(
-                            children: [
-                              Image(
-                                image: AssetImage("assets/login.png"),
-                              ),
-                              SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                "Title",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Colors.black,
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "Title",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            "discription dsqiybfd zyebf zieybf zfyvbrze azbdfe zyefvb'z ",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 10),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          MaterialButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => buttomNavigation(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "Consulter",
+                              style: TextStyle(
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 15,
-                                ),
-                              ),
-                              Text(
-                                "discription dsqiybfd zyebf zieybf zfyvbrze azbdfe zyefvb'z ",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(fontSize: 10),
-                              ),
-                              SizedBox(
-                                height: 15,
-                              ),
-                              MaterialButton(
-                                onPressed: () {
-                                  Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (_) => buttomNavigation(),
-                                    ),
-                                  );
-                                },
-                                child: Text(
-                                  "Consulter",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 15),
-                                ),
-                                color: Colors.blue,
-                              ),
-                            ],
+                                  color: Colors.white,
+                                  fontSize: 15),
+                            ),
+                            color: Colors.blue,
                           ),
-                        ),
-                        SizedBox(
-                          width: 30,
-                        ),
-                        Container(
-                          height: 280,
-                          width: 150,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.green),
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Color.fromARGB(255, 160, 232, 162)),
-                            ],
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 350,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.green),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(color: Color.fromARGB(255, 160, 232, 162)),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Image(
+                            image: AssetImage("assets/login.png"),
                           ),
-                          child: Column(
-                            children: [
-                              Image(
-                                image: AssetImage("assets/login.png"),
-                              ),
-                              SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                "Title",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Colors.black,
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            "Title",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            "discription dsqiybfd zyebf zieybf zfyvbrze azbdfe zyefvb'z ",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 10),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          MaterialButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => buttomNavigation(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "Consulter",
+                              style: TextStyle(
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 15,
-                                ),
-                              ),
-                              Text(
-                                "discription dsqiybfd zyebf zieybf zfyvbrze azbdfe zyefvb'z ",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(fontSize: 10),
-                              ),
-                              SizedBox(
-                                height: 15,
-                              ),
-                              MaterialButton(
-                                onPressed: () {
-                                  Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (_) => buttomNavigation(),
-                                    ),
-                                  );
-                                },
-                                child: Text(
-                                  "Consulter",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
-                                      fontSize: 15),
-                                ),
-                                color: Colors.blue,
-                              ),
-                            ],
+                                  color: Colors.white,
+                                  fontSize: 15),
+                            ),
+                            color: Colors.blue,
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
                     ),
                   ],
                 ),
